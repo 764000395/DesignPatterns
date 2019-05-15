658d114192264c7987c94986b983fca3eb2b13cd
YangZhiying <764000395@qq.com>
2019-05-15 14:50:47 +0800

    工厂模式

--- Dd/Factory.php
+++ Dd/Factory.php
@@ -13,6 +13,10 @@ class Factory
 {
     static function createDatabase(){
         $db = Databases::getInstance();
+
+        // 工厂方法中生产类的实例的时候将其添加到注册树
+        Register::set('db1', $db);
+
         return $db;
     }
 }
