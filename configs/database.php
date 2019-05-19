d8f66544290485eb2f70447be5c780a179ae3e8f
YangZhiying <764000395@qq.com>
2019-05-19 18:03:51 +0800

    配置文件

--- configs/database.php
+++ configs/database.php
@@ -5,3 +5,31 @@
  * Date: 2019-05-19
  * Time: 16:49
  */
+$config = array(
+    'master' => array(
+        'type' => 'MySQL',
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => 'design123',
+        'db_name' => 'design',
+    ),
+
+    'slave' => array(
+        'slave1' => array(
+            'type' => 'MySQL',
+            'host' => 'localhost',
+            'username' => 'root',
+            'password' => 'design123',
+            'db_name' => 'design',
+        ),
+
+        'slave2' => array(
+            'type' => 'MySQL',
+            'host' => 'localhost',
+            'username' => 'root',
+            'password' => 'design123',
+            'db_name' => 'design',
+        ),
+    )
+);
+return $config;
