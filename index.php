e864cff391bf871c86fdfb3b208df51efc3b878e
YangZhiying <764000395@qq.com>
2019-05-19 14:52:29 +0800

    入口文件

--- index.php
+++ index.php
@@ -5,3 +5,10 @@
  * Date: 2019-05-19
  * Time: 12:23
  */
+define("BASEDIR", __DIR__);
+
+require BASEDIR . '/Dd/Loader.php';
+spl_autoload_register('\\Dd\\Loader::autoload');
+
+$config = new \Dd\Config(BASEDIR . '/configs');
+var_dump($config['controller']);
